8717ad3f175630981cf6dc67a50b391df0cd3815
Benjy <ben@pngs.cc>
2024-06-18 22:05:07 +0800

    feat: toggleterm

---
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        direction = 'float',
+        size = 20
+      })
+      vim.keymap.set('n', '<leader>tt', ':ToggleTerm<CR>')
+      vim.keymap.set('t', '<esc>', '<C-\\><C-n>:ToggleTerm<CR>')
+    end
+  }
+}
