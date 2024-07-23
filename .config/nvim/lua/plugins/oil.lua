4bb27ffa2ee81287b3b7609e68c18b787e10be95
benjypng <47936793+benjypng@users.noreply.github.com>
2024-07-24 07:48:35 +0800

    nvim: show hidden

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -5,6 +5,14 @@ return {
   -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
   dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   config = function()
-    require("oil").setup()
+    require("oil").setup({
+      columns = {
+        "icon",
+        "size"
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    })
   end
 }
