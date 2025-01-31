fbf74246ba6000a708710a9efbf1ec8727f242c4
benjypng <47936793+benjypng@users.noreply.github.com>
2025-01-31 18:49:02 +0800

    nvim: change to mini-cmp

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -96,7 +96,6 @@ return {
       })
 
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
       capabilities.textDocument.foldingRange = {
         dynamicRegistration = false,
--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -18,6 +18,46 @@ return {
 
       require('mini.pairs').setup()
 
+      require('mini.completion').setup {
+        delay = { completion = 150, info = 200, signature = 250 },
+        window = { info = {}, signature = {} },
+        lsp_completion = {
+          source_func = 'omnifunc',
+          auto_setup = true,
+        },
+        mappings = {
+          force_twostep = '<C-Space>',
+          force_fallback = '<C-f>',
+        },
+      }
+      require('mini.snippets').setup {}
+      local imap_expr = function(lhs, rhs)
+        vim.keymap.set('i', lhs, rhs, { expr = true })
+      end
+      imap_expr('<Tab>', [[pumvisible() ? "\<C-n>" : "\<Tab>"]])
+      imap_expr('<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]])
+
+      -- Handle Enter
+      local keycode = vim.keycode or function(x)
+        return vim.api.nvim_replace_termcodes(x, true, true, true)
+      end
+      local keys = {
+        ['cr'] = keycode '<CR>',
+        ['ctrl-y'] = keycode '<C-y>',
+        ['ctrl-y_cr'] = keycode '<C-y><CR>',
+      }
+
+      _G.cr_action = function()
+        if vim.fn.pumvisible() ~= 0 then
+          local item_selected = vim.fn.complete_info()['selected'] ~= -1
+          return item_selected and keys['ctrl-y'] or keys['ctrl-y_cr']
+        else
+          return keys['cr']
+        end
+      end
+
+      vim.keymap.set('i', '<CR>', 'v:lua._G.cr_action()', { expr = true, noremap = true, silent = true })
+
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
