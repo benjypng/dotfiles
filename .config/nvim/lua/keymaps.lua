44b4833b3aa1bd48b0a7d12425e81230cc6e9660
benjypng <47936793+benjypng@users.noreply.github.com>
2025-02-09 16:14:36 +0800

    nvim: echo save upon ;;

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -14,7 +14,7 @@ vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Save All
-map('n', ';;', ':wall<CR>:echo "SAVED SAVED SAVED"')
+map('n', ';;', ':wall<CR>:echo "SAVED SAVED SAVED"<CR>')
 
 -- Quit
 map('n', '<leader>q', ':q<CR>')
