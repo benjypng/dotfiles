67e08aa5e833b20d07577a248c1a080bd3d2ed17
benjypng <47936793+benjypng@users.noreply.github.com>
2025-09-15 22:16:37 +0800

    sketchybar and aerospace

--- .config/sketchybar/plugins/battery.sh
+++ .config/sketchybar/plugins/battery.sh
@@ -1,29 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 
-BATT_PERCENT=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
-CHARGING=$(pmset -g batt | grep 'AC Power')
+PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
+CHARGING="$(pmset -g batt | grep 'AC Power')"
 
-sketchybar --set ${NAME} icon.color=0xff989898
-
-if [[ ${CHARGING} != "" ]]; then
-	sketchybar --set ${NAME} icon="􀢋"
-	sketchybar --set ${NAME} label="${BATT_PERCENT}%"
-	exit 0
+if [ "$PERCENTAGE" = "" ]; then
+  exit 0
 fi
 
-case ${BATT_PERCENT} in
-100) ICON="􀛨" ;;
-9[0-9]) ICON="􀛨" ;;
-8[0-9]) ICON="􀛨" ;;
-7[0-9]) ICON="􀺸" ;;
-6[0-9]) ICON="􀺸" ;;
-5[0-9]) ICON="􀺶" ;;
-4[0-9]) ICON="􀺶" ;;
-3[0-9]) ICON="􀛩" ;;
-2[0-9]) ICON="􀛩" ;;
-1[0-9]) ICON="􀛪" ;;
-*) ICON="􀢋" ;;
+case "${PERCENTAGE}" in
+  9[0-9]|100) ICON=""
+  ;;
+  [6-8][0-9]) ICON=""
+  ;;
+  [3-5][0-9]) ICON=""
+  ;;
+  [1-2][0-9]) ICON=""
+  ;;
+  *) ICON=""
 esac
 
-sketchybar --set ${NAME} icon="${ICON}"
-sketchybar --set ${NAME} label="${BATT_PERCENT}%"
+if [[ "$CHARGING" != "" ]]; then
+  ICON=""
+fi
+
+# The item invoking this script (name $NAME) will get its icon and label
+# updated with the current battery status
+sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%"
