d0eec95cd261bd225d2c8c389bdc9ced510892e9
benjypng <47936793+benjypng@users.noreply.github.com>
2025-12-18 20:51:11 +0800

    nvim: codecompanion keymap

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -10,7 +10,7 @@ return {
   keys = {
     {
       '<leader>cc',
-      '<cmd>CodeCompanionChat<CR>',
+      '<cmd>CodeCompanionChat Toggle<CR>',
       desc = 'Open CodeCompanion Chat',
     },
   },
