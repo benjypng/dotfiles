8f498e5f596acf26c8e62c48884836d881450cbe
benjypng <47936793+benjypng@users.noreply.github.com>
2025-12-19 13:43:50 +0800

    nvim: themes

---
+++ .config/nvim/lua/plugins/themes/rose-pine.lua
@@ -0,0 +1,12 @@
+return {
+  'rose-pine/neovim',
+  name = 'rose-pine',
+  init = function()
+    require('rose-pine').setup {
+      -- variant = 'moon',
+      variant = 'dawn',
+    }
+    vim.cmd.colorscheme 'rose-pine-dawn'
+    vim.cmd.hi 'LineNr guifg = #FFFFFF'
+  end,
+}
