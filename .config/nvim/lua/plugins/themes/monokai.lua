aae11fb58e829dcf10acf9a4ebfb3c8968bd102e
benjypng <47936793+benjypng@users.noreply.github.com>
2026-03-21 22:25:44 +0800

    nvim: theme

--- .config/nvim/lua/plugins/themes/monokai.lua
+++ .config/nvim/lua/plugins/themes/monokai.lua
@@ -3,7 +3,7 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    require('monokai-pro').setup()
-    vim.cmd.colorscheme 'monokai-pro'
+--    require('monokai-pro').setup()
+--    vim.cmd.colorscheme 'monokai-pro'
   end,
 }
