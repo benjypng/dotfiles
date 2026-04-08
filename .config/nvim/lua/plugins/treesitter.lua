95ca1456bb16b797ff32eeda373bdb4b623831c3
benjypng <47936793+benjypng@users.noreply.github.com>
2026-04-08 13:54:26 +0800

    nvim: treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,5 @@
 return {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    version = false,
-    build = ':TSUpdate',
-    branch = 'master',
-    opts = {
-      ensure_installed = { 'typescript', 'javascript', 'lua' },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        disable = function(_, buf)
-          local max_filesize = 1024 * 1024
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
-        end,
-      },
-      indent = { enable = true, disable = { 'ruby', 'html' } },
-    },
-    config = function(_, opts)
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
-    end,
-  },
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate'
 }
