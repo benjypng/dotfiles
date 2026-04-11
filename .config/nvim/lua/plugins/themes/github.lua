3a6f9951398e9a163bb5ac4b530f0d47abd35b53
benjypng <47936793+benjypng@users.noreply.github.com>
2026-04-11 20:48:45 +0800

    -

--- .config/nvim/lua/plugins/themes/github.lua
+++ .config/nvim/lua/plugins/themes/github.lua
@@ -1,20 +1,11 @@
-local hour = tonumber(os.date '%H')
-local theme
-
 return {
   'projekt0n/github-nvim-theme',
   name = 'github-theme',
   lazy = false,
   priority = 1000,
   config = function()
-    require('github-theme').setup {}
+    --   require('github-theme').setup {}
 
-    if hour >= 8 and hour < 18 then
-      theme = 'github_light_default'
-    else
-      theme = 'github_dark_default'
-    end
-
-    vim.cmd.colorscheme(theme)
+    --   vim.cmd.colorscheme 'github_light_default'
   end,
 }
