236a602784ec8e932719e9bdaf9d258b5345afc1
benjypng <47936793+benjypng@users.noreply.github.com>
2026-05-03 08:15:36 +0800

    nvim: transparency

--- .config/nvim/lua/plugins/themes/monokai.lua
+++ .config/nvim/lua/plugins/themes/monokai.lua
@@ -3,7 +3,21 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    require('monokai-pro').setup()
+    require('monokai-pro').setup {
+      transparent_background = true,
+    }
     vim.cmd.colorscheme 'monokai-pro'
+
+    local groups = {
+      'Normal', 'NormalNC', 'NormalFloat', 'FloatBorder', 'FloatTitle',
+      'SignColumn', 'LineNr', 'EndOfBuffer', 'VertSplit', 'WinSeparator',
+      'StatusLine', 'StatusLineNC', 'TabLine', 'TabLineFill',
+      'NeoTreeNormal', 'NeoTreeNormalNC', 'NeoTreeEndOfBuffer',
+      'TelescopeNormal', 'TelescopeBorder',
+      'WhichKeyFloat',
+    }
+    for _, g in ipairs(groups) do
+      vim.api.nvim_set_hl(0, g, { bg = 'NONE' })
+    end
   end,
 }
