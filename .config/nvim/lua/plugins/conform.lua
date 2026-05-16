94f2683436130aff81806ef594823495a43c18ba
benjypng <47936793+benjypng@users.noreply.github.com>
2026-05-16 15:13:01 +0800

    fix: conform

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -28,7 +28,7 @@ return {
   opts = {
     notify_on_error = false,
     format_on_save = {
-      timeout_ms = 500,
+      timeout_ms = 1000,
       lsp_format = 'never',
     },
     formatters_by_ft = {
